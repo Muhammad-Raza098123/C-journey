aa95beb7e2a928804e801c0caa6ac1f3a959128b
Muhammad Raza <muhammadkhoso098123@gmail.com>
2026-09-18 00:14:03 +0500

    Update sample lab task 4 question 3.cpp

--- sample lab task 4 question 3.cpp
+++ sample lab task 4 question 3.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main(){
 	int a,b;
+/* Using bitwise right-shift (>>) for division by 4. 
+   Shifting right by 2 positions (num >> 2) is equivalent to 
+   truncating integer division by 2^2 (4). */
 	printf("Enter First Postive Number: ");
 	scanf("%d",&a);
 	printf("\nNow Enter Second Positive Number: ");
